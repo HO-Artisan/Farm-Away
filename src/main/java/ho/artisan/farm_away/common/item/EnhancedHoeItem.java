2018971a4543ad8fcb121868197bc7afa1d6d69c
RawDiamondMC <RawDiamondMC@outlook.com>
2024-07-22 15:35:38 +0800

    hoe

--- src/main/java/ho/artisan/farm_away/common/item/EnhancedHoeItem.java
+++ src/main/java/ho/artisan/farm_away/common/item/EnhancedHoeItem.java
@@ -28,17 +28,29 @@ public class EnhancedHoeItem extends HoeItem {
 	protected static final Map<Block, Pair<Predicate<UseOnContext>, Consumer<UseOnContext>>> TILLING_ACTIONS = Maps.newHashMap(
 		ImmutableMap.of(
 			Blocks.STONE,
-			Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(FABlocks.STONE_FARMLAND.get().defaultBlockState())),
+			Pair.of(HoeItem::onlyIfAirAbove, successful(FABlocks.STONE_FARMLAND.get().defaultBlockState())),
 			Blocks.NETHERRACK,
-			Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(FABlocks.NETHERRACK_FARMLAND.get().defaultBlockState())),
+			Pair.of(HoeItem::onlyIfAirAbove, successful(FABlocks.NETHERRACK_FARMLAND.get().defaultBlockState())),
 			Blocks.END_STONE,
-			Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(FABlocks.END_STONE_FARMLAND.get().defaultBlockState())),
+			Pair.of(HoeItem::onlyIfAirAbove, successful(FABlocks.END_STONE_FARMLAND.get().defaultBlockState())),
 			Blocks.GRANITE,
-			Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(FABlocks.GRANITE_FARMLAND.get().defaultBlockState())),
+			Pair.of(HoeItem::onlyIfAirAbove, successful(FABlocks.GRANITE_FARMLAND.get().defaultBlockState())),
 			Blocks.DIORITE,
-			Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(FABlocks.DIORITE_FARMLAND.get().defaultBlockState())),
+			Pair.of(HoeItem::onlyIfAirAbove, successful(FABlocks.DIORITE_FARMLAND.get().defaultBlockState())),
 			Blocks.ANDESITE,
-			Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(FABlocks.ANDESITE_FARMLAND.get().defaultBlockState()))
+			Pair.of(HoeItem::onlyIfAirAbove, successful(FABlocks.ANDESITE_FARMLAND.get().defaultBlockState()))
+		)
+	);
+	protected static final Map<Block, Pair<Predicate<UseOnContext>, Consumer<UseOnContext>>> VANILLA_TILLING_ACTIONS = Maps.newHashMap(
+		ImmutableMap.of(
+			Blocks.GRASS_BLOCK,
+			Pair.of(HoeItem::onlyIfAirAbove, fail()),
+			Blocks.DIRT_PATH,
+			Pair.of(HoeItem::onlyIfAirAbove, fail()),
+			Blocks.DIRT,
+			Pair.of(HoeItem::onlyIfAirAbove, fail()),
+			Blocks.COARSE_DIRT,
+			Pair.of(HoeItem::onlyIfAirAbove, fail())
 		)
 	);
 
@@ -48,23 +60,18 @@ public class EnhancedHoeItem extends HoeItem {
 
 	@Override
 	public InteractionResult useOn(UseOnContext context) {
+		Map<Block, Pair<Predicate<UseOnContext>, Consumer<UseOnContext>>> map = Maps.newHashMap(TILLING_ACTIONS);
+		map.putAll(VANILLA_TILLING_ACTIONS);
 		Level level = context.getLevel();
 		BlockPos blockPos = context.getClickedPos();
-		Pair<Predicate<UseOnContext>, Consumer<UseOnContext>> pair = TILLING_ACTIONS.get(level.getBlockState(blockPos).getBlock());
+		Pair<Predicate<UseOnContext>, Consumer<UseOnContext>> pair = map.get(level.getBlockState(blockPos).getBlock());
 		if (pair == null) {
 			return InteractionResult.PASS;
 		} else {
 			Predicate<UseOnContext> predicate = pair.getFirst();
 			Consumer<UseOnContext> consumer = pair.getSecond();
 			if (predicate.test(context)) {
-				Player player = context.getPlayer();
-				level.playSound(player, blockPos, SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-				if (!level.isClientSide) {
-					consumer.accept(context);
-					if (player != null) {
-						context.getItemInHand().hurtAndBreak(1, player, LivingEntity.getSlotForHand(context.getHand()));
-					}
-				}
+				consumer.accept(context);
 				return InteractionResult.sidedSuccess(level.isClientSide);
 			} else {
 				return InteractionResult.PASS;
@@ -72,10 +79,37 @@ public class EnhancedHoeItem extends HoeItem {
 		}
 	}
 
-	public static Consumer<UseOnContext> changeIntoState(BlockState blockState) {
+	public static Consumer<UseOnContext> successful(BlockState blockState) {
 		return useOnContext -> {
-			useOnContext.getLevel().setBlock(useOnContext.getClickedPos(), blockState, 11);
-			useOnContext.getLevel().gameEvent(GameEvent.BLOCK_CHANGE, useOnContext.getClickedPos(), GameEvent.Context.of(useOnContext.getPlayer(), blockState));
+			Level level = useOnContext.getLevel();
+			BlockPos blockPos = useOnContext.getClickedPos();
+			Player player = useOnContext.getPlayer();
+			level.playSound(player, blockPos, SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
+			if (level.isClientSide) {
+				return;
+			}
+			level.setBlock(blockPos, blockState, 11);
+			level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, blockState));
+			if (player != null) {
+				useOnContext.getItemInHand().hurtAndBreak(1, player, LivingEntity.getSlotForHand(useOnContext.getHand()));
+			}
+		};
+	}
+
+	public static Consumer<UseOnContext> fail() {
+		return useOnContext -> {
+			Level level = useOnContext.getLevel();
+			BlockPos blockPos = useOnContext.getClickedPos();
+			Player player = useOnContext.getPlayer();
+			level.playSound(player, blockPos, SoundEvents.GRAVEL_BREAK, SoundSource.BLOCKS, 1.0F, 1.0F);
+			if (level.isClientSide) {
+				return;
+			}
+			level.setBlock(blockPos, Blocks.AIR.defaultBlockState(), 11);
+			level.gameEvent(GameEvent.BLOCK_DESTROY, blockPos, GameEvent.Context.of(player, level.getBlockState(blockPos)));
+			if (player != null) {
+				useOnContext.getItemInHand().hurtAndBreak(3, player, LivingEntity.getSlotForHand(useOnContext.getHand()));
+			}
 		};
 	}
 }
