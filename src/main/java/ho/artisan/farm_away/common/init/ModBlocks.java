17a6f2332e128e6474677b6574a155222cc2e8c5
RawDiamondMC <RawDiamondMC@outlook.com>
2024-07-22 13:21:38 +0800

    BlockItems

--- src/main/java/ho/artisan/farm_away/common/init/ModBlocks.java
+++ src/main/java/ho/artisan/farm_away/common/init/ModBlocks.java
@@ -5,18 +5,17 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
-
-import java.util.function.Supplier;
 
 @SuppressWarnings("unused")
 public class ModBlocks {
     private static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks("farm_away");
 
     // Farmlands
-    public static final Supplier<Block> STONE_FARMLAND = register("stone_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.STONE), FarmlandBlock.Type.TERRA);
-    public static final Supplier<Block> NETHERRACK_FARMLAND = register("netherrack_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERRACK), FarmlandBlock.Type.FLAME);
-    public static final Supplier<Block> END_STONE_FARMLAND = register("end_stone_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.END_STONE), FarmlandBlock.Type.SHADOW);
+    public static final DeferredBlock<Block> STONE_FARMLAND = register("stone_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.STONE), FarmlandBlock.Type.TERRA);
+    public static final DeferredBlock<Block> NETHERRACK_FARMLAND = register("netherrack_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERRACK), FarmlandBlock.Type.FLAME);
+    public static final DeferredBlock<Block> END_STONE_FARMLAND = register("end_stone_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.END_STONE), FarmlandBlock.Type.SHADOW);
     //public static final Supplier<Block> DEEPSLATE_FARMLAND = BLOCKS.registerSimpleBlock("name", BlockBehaviour.Properties.of());
     //public static final Supplier<Block> BASALT_FARMLAND = BLOCKS.registerSimpleBlock("name", BlockBehaviour.Properties.of());
     //public static final Supplier<Block> BLACKSTONE_FARMLAND = BLOCKS.registerSimpleBlock("name", BlockBehaviour.Properties.of());
@@ -24,11 +23,11 @@ public class ModBlocks {
     //public static final Supplier<Block> CALCITE_FARMLAND = BLOCKS.registerSimpleBlock("name", BlockBehaviour.Properties.of());
     //public static final Supplier<Block> TUFF_FARMLAND = BLOCKS.registerSimpleBlock("name", BlockBehaviour.Properties.of());
     //public static final Supplier<Block> SCULK_FARMLAND = BLOCKS.registerSimpleBlock("name", BlockBehaviour.Properties.of());
-    public static final Supplier<Block> GRANITE_FARMLAND = register("granite_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.GRANITE), FarmlandBlock.Type.SCARLET);
-    public static final Supplier<Block> DIORITE_FARMLAND = register("diorite_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.DIORITE), FarmlandBlock.Type.RAY);
-    public static final Supplier<Block> ANDESITE_FARMLAND = register("andesite_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.ANDESITE), FarmlandBlock.Type.SHADOW);
+    public static final DeferredBlock<Block> GRANITE_FARMLAND = register("granite_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.GRANITE), FarmlandBlock.Type.SCARLET);
+    public static final DeferredBlock<Block> DIORITE_FARMLAND = register("diorite_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.DIORITE), FarmlandBlock.Type.RAY);
+    public static final DeferredBlock<Block> ANDESITE_FARMLAND = register("andesite_farmland",BlockBehaviour.Properties.ofFullCopy(Blocks.ANDESITE), FarmlandBlock.Type.SHADOW);
 
-    private static Supplier<Block> register(String name, BlockBehaviour.Properties properties, FarmlandBlock.Type type) {
+    private static DeferredBlock<Block> register(String name, BlockBehaviour.Properties properties, FarmlandBlock.Type type) {
         return BLOCKS.register(name, () -> new FarmlandBlock(properties, type));
     }
 
